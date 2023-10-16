771b8d078474d6114a1cb7613f6d574e806adee0
YK <yacob.kazal@qantas.com.au>
2023-10-16 14:50:03 +1100

    Initial Commit

---
+++ SocialPhotograph/SocialPhotographApp.swift
@@ -0,0 +1,20 @@
+//
+//  SocialPhotographApp.swift
+//  SocialPhotograph
+//
+//  Created by yacob kazal on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialPhotographApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
